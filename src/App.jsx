79e7a6a4f9314a374d970bd0ba50b521fc2a0fe9
Adeoti Opeyemi <adeotijamiu95@gmail.com>
2026-05-08 15:01:03 +0100

    fix

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Leraners from './components/leraners'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Leraners />
+    </div>
+  )
+}
