ecd30cc5fd8f31bc3a847f476187bac44b891a97
Adeoti Opeyemi <adeotijamiu95@gmail.com>
2026-05-08 13:48:23 +0100

    changes

---
+++ public/components/leraners.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const leraners = () => {
+  return (
+    <div>
+        <h1>Build a smarter school experience in minute</h1> 
+        <p>from lessson planning to students engagement-everything in one place.Teach better.Manage easier</p>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default leraners
