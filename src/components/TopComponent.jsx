5c86281bf854b9c4f6e79c3f263210ec35a05f29
isaacdemilade8-creator <isaacdemilade8@gmail.com>
2026-05-08 13:21:59 +0100

    demi commit

---
+++ src/components/TopComponent.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+import { IoMdLogOut } from "react-icons/io";
+
+
+const TopComponent = () => {
+  return (
+    <div>
+        <div><span>Having troubles? <Link>Get Help</Link></span> <IoMdLogOut />Logout</div>
+    </div>
+  )
+}
+
+export default TopComponent
